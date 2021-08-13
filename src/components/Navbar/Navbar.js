10dfbe3151f1048e23f5550b69d67aafda9829e5
jhhg0 <jhherrera@ccss.sa.cr>
2021-08-13 17:18:15 -0500

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 6rem;
+`;
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
